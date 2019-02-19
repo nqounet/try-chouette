db96094e676e516ea6455ac2ad95291781d3b0d9
nqounet <mail@nqou.net>
2019-02-20 03:31:23 +0900

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Try::Chouette - It's new $module
+
+# SYNOPSIS
+
+    use Try::Chouette;
+
+# DESCRIPTION
+
+Try::Chouette is ...
+
+# LICENSE
+
+Copyright (C) nqounet.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+nqounet <mail@nqou.net>
---
+++ lib/Try/Chouette.pm
@@ -0,0 +1,39 @@
+package Try::Chouette;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Try::Chouette - It's new $module
+
+=head1 SYNOPSIS
+
+    use Try::Chouette;
+
+=head1 DESCRIPTION
+
+Try::Chouette is ...
+
+=head1 LICENSE
+
+Copyright (C) nqounet.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+nqounet E<lt>mail@nqou.netE<gt>
+
+=cut
+
